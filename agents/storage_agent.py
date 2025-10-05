f5411a09f30c97d68e097275125db65f576761d1
Jun Jiang <jiangjun4@sina.com>
2025-10-05 13:46:49 +0800

    binding agent

--- agents/storage_agent.py
+++
@@ -1,9 +0,0 @@
-# agents/storage_agent.py
-from utils.logger import logger
-
-class StorageAgent:
-    def save_stock_data(self, df):
-        pass
-
-    def save_news_data(self, df):
-        pass
